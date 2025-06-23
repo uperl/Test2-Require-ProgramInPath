9fda6a928391c13d9705375094ffca51558fa004
Graham Ollis <plicease@cpan.org>
2025-06-23 12:49:00 -0600

    Initial structure

---
+++ lib/Test2/Require/ProgramInPath.pm
@@ -0,0 +1,9 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+use true;
+
+package Test2::Require::ProgramInPath {
+
+    # ABSTRACT: Skip test unless a program exists in the PATH
+}
---
+++ t/test2_require_programinpath.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Test2::Require::ProgramInPath;
+
+ok 1, 'todo';
+
+done_testing;
